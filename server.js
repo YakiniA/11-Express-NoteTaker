4e3da19bcf7e3945db5198253b450f98739e84c0
YakiniA <yakinilatha09@gmail.com>
2020-08-03 09:00:54 -0600

    Opening localhost

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
