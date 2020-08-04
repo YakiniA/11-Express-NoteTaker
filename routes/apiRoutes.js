5557510d572b37c37b625a3a3d7de76368ca4ff2
YakiniA <yakinilatha09@gmail.com>
2020-08-04 10:38:25 -0600

    new notes- added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,7 @@ console.log(__dirname);
 var pathForJSON = path.join(__dirname, '../db/db.json');
 console.log(pathForJSON);
 
+
 module.exports = function(app) {
 
 fs.readFile(pathForJSON, "utf8", function(err, data) {
@@ -15,11 +16,32 @@ fs.readFile(pathForJSON, "utf8", function(err, data) {
     throw err;
   }
   console.log(data);
-  // Parse the JSON string to an object
-  console.log(JSON.stringify(data, null, 2));
+ 
+
   app.get("/api/notes", function(req, res) {
-    res.json(data);
+    res.json(JSON.parse(data));
   });
 });
+
+
+  app.post("/api/notes", function(req, res) {
+   // JSON.stringify(data, null, '\t')
+   console.log(req.body);
+  var storedData = [];
+storedData.push(JSON.stringify(req.body));
+console.log(storedData);
+fs.appendFile(pathForJSON,  storedData, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+    
+        res.json(true);
+        console.log("Success!");
+    
+      });
+   
+      
+  });
+  
 }
 
