8e907b4e11126240734fa8cca77041035c53c2b8
YakiniA <yakinilatha09@gmail.com>
2020-08-04 23:13:07 -0600

    Added apiRoutes,htmlRoutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,44 @@
+
+const fs = require("fs");
+const path = require("path");
+// var db = require("../db/db.json");
+
+// ROUTING
+console.log(__dirname);
+var pathForJSON = path.join(__dirname, '../db/db.json');
+console.log(pathForJSON);
+
+ module.exports = function(app) {
+
+ fs.readFile(pathForJSON, "utf8", function(err, data) {
+   if (err) {
+     throw err;
+   }
+    console.log(JSON.parse(data));
+    console.log(data);
+  
+    app.get("/api/notes", function(req, res) {
+      return res.json(JSON.parse(data));
+    }); 
+ });
+
+
+   app.post("/api/notes", function(req, res) {
+     JSON.stringify(data, null, '\t')
+    console.log(req.body);
+    var storedData = [];
+    storedData.push(JSON.stringify(req.body));
+    console.log(storedData);
+    fs.appendFile(pathForJSON,  storedData, function(err) {
+         if (err) {
+           return console.log(err);
+         }
+    
+         res.json(true);
+         console.log("Success!");
+    
+       }); 
+   });
+  
+ }
+
--- server.js
+++ server.js
@@ -2,15 +2,10 @@
 // DEPENDENCIES
 
 var express = require("express");
-var path = require("path");
-var fs = require("fs");
-console.log(__dirname);
-var indexJSFile = require("./assets/js/index.js");
+
 // EXPRESS CONFIGURATION
 
 var app = express();
-
-var pathForJSON = path.join(__dirname, './db/db.json');
 
 // Sets an initial port
 var PORT = process.env.PORT || 8080;
@@ -19,30 +14,9 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
-});
-
-
-// If no matching route is found default to home
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
-});
-
-fs.readFile(pathForJSON, "utf8", function(err, data) {
-  if (err) {
-    throw err;
-  }
-   var parsedData = JSON.parse(data);
-  console.log(parsedData);
-  // var dataParsed = $.parseJSON(data);
-
-  indexJSFile.renderNoteList(data);
-
 });
